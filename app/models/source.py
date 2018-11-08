48753c051a8396416ba8db750eee91ebe0241c36
Chris-karanja <chriswainaina0@gmail.com>
2018-11-09 00:48:20 +0300

    added test methods to the SourceNewsTest file

--- app/models/source.py
+++ app/models/source.py
@@ -7,4 +7,16 @@ class Source:
         self.url = url
         self.category = category
         self.language = language
-        self.country = country 
+        self.country = country
+
+class Source_News:
+    ''' source_news to define objects of news from various sources '''
+
+    def __init__(self,source_id,author,title,description,url,urlToImage,publishedAt):
+        self.source_id = source_id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
--- app/models/source_test.py
+++ app/models/source_test.py
@@ -1,6 +1,7 @@
 import unittest
 from .models import source
 Source = source.Source
+Source_News = source.Source_News
 
 class SourceTest(unittest.TestCase):
     ''' test class to test behavior of Source class '''
@@ -12,6 +13,21 @@ class SourceTest(unittest.TestCase):
     def test_instance(self):
         ''' checks if object self.new_source is an instance of Source class '''
         self.assertTrue(isinstance(self.new_source,Source))
+
+        	
+            
+class SourceNewsTest(unittest.TestCase):
+    ''' test class to test behavior of Source class '''
+    
+    def setUp(self):
+        ''' set up method that will run before every test '''
+
+        self.new_source_news = Source_News("Kay", "Kay news", "bananas are awesome", "Minions love bananas", "www.kay.com/articles", "www.kay.com/photos/sdfkjd", "2018-10-01T20:47:36Z")
+    
+    def test_instance(self):
+        ''' checks if object self.new_source is an instance of Source class '''
+       
+        self.assertTrue(isinstance(self.new_source_news,Source_News))
         
 if __name__ == '__main__':
     unittest.main()
