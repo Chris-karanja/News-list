d20421b9299794251918c30c9be966e3642e4d2f
Chris-karanja <chriswainaina0@gmail.com>
2018-11-08 11:50:58 +0300

    added instance config with api key to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from .config import DevConfig
-app = Flask(__name__)	
- # config 
+
+app = Flask(__name__,instance_relative_config = True)
+
+# config 
 app.config.from_object(DevConfig)
-from app import views 	
+app.config.from_pyfile('config.py')
+
+from app import views
