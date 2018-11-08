0b4b2f435a23caf302ed835571d3b69f5e40df5f
Chris-karanja <chriswainaina0@gmail.com>
2018-11-08 23:28:08 +0300

    passed sources to render template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
+from request import get_sources
 
  #views
 @app.route('/')
 def index():
     ''' returns index page and its data '''
-     return render_template('index.html')
+    
+    #getting movie sources
+    news_sources = get_sources('sources')
+    print(news_sources)
+
+    return render_template('index.html', sources = news_sources)
