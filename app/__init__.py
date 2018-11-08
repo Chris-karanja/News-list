eea933fc6c97024baef8ce33c9e8a01f0679fa0e
Chris-karanja <chriswainaina0@gmail.com>
2018-11-09 00:35:14 +0300

    installed bootstrap and created navbar.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__,instance_relative_config = True)
+# app init
+app = Flask(__name__,instance_relative_config = True)	
 
 # config 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Init Flask Ext
+bootstrap = Bootstrap(app)
 
 from app import views
