b0c58af18e69af936239f4b742c9be44a69fa29a
Chris-karanja <chriswainaina0@gmail.com>
2018-11-09 00:56:16 +0300

    added Source_News model, get_source_news and process_source_news methods

--- request.py
+++ request.py
@@ -3,6 +3,7 @@ import urllib.request,json
 from app.models import source
 
 Source = source.Source
+Source_News = source.Source_News
 
 #get api key
 api_key = app.config['SOURCE_API_KEY']
@@ -54,3 +55,36 @@ def process_sources(source_list):
     
     
     return source_results
+
+def get_source_news(news_source):
+    ''' json to request to display news from a source id '''
+    get_source_news_url = base_url.format(id,api_key)
+    with urllib.request.urlopen(get_source_news_url) as url:
+        source_news_data = url.read()
+        source_news_response = json.loads(source_news_data)
+         source_news_results = None
+        if source_news_response['articles']:
+            source_news_results = process_source_news(source_news_response['articles'])
+    return source_news_results
+
+# process results from get_source_news
+def process_source_news(fetched_articles):
+    ''' method to process json formatted article data '''
+
+    source_news_list = []
+
+    for article in fetched_articles:
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        urlToImage = article.get('urlToImage')
+        publishedAt = article.get('publishedAt')
+
+        if urlToImage:
+            source_news_object = Source_News(author,title,description,url,urlToImage,publishedAt)
+            source_news_list.append(source_news_object)
+    return source_news_list 
+
+
+    
