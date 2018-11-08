b48a662e688805697a202e0a17050cd78b39b759
Chris-karanja <chriswainaina0@gmail.com>
2018-11-08 20:58:12 +0300

    created the get_source and process_sources

--- app/models/source_test.py
+++ app/models/source_test.py
@@ -1,14 +1,17 @@
-mport unittest
+import unittest
 from .models import source
 Source = source.Source
- class SourceTest(unittest.TestCase):
+
+class SourceTest(unittest.TestCase):
     ''' test class to test behavior of Source class '''
-     def setUp(self):
+
+    def setUp(self):
         ''' set up method that will run before every test '''
-         self.new_source = Source("Kay", "Kay news", "Daily News plug", "https://www.kay.com", "news", "english", "ke")
-     def test_instance(self):
-         ''' checks if object self.new_source is an instance of Source class '''
-         self.assertTrue(isinstance(self.new_source,Source))
-     
+        self.new_source = Source("Kay", "Kay news", "Daily News plug", "https://www.kay.com", "news", "english", "ke")
+
+    def test_instance(self):
+        ''' checks if object self.new_source is an instance of Source class '''
+        self.assertTrue(isinstance(self.new_source,Source))
+        
 if __name__ == '__main__':
     unittest.main()
